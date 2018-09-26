fcfb382e2cf089482b0b5ef2bd4a31817e0ee68a
Miracle <aertius@gmail.com>
2018-09-26 23:32:55 +0600

    add solutions to exercises 1.2 and 1.3

---
+++ exercises/1.3.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require rackunit)
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (sum-of-squares-max-two-of-three a b c)
+  (cond ((< a b) (if (< a c) (sum-of-squares b c) (sum-of-squares b a)))
+        (else (if (< b c) (sum-of-squares a c) (sum-of-squares a b)))))
+
+(check-equal? (sum-of-squares-max-two-of-three 1 2 3) 13)
+(check-equal? (sum-of-squares-max-two-of-three -100 -1 0) 1)
+(check-equal? (sum-of-squares-max-two-of-three -1000 0 0) 0)
+(check-equal? (sum-of-squares-max-two-of-three -1000 -5.5 100) 10030.25)
+(check-equal? (sum-of-squares-max-two-of-three 3 3 3) 18)
