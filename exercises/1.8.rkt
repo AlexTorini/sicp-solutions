fc291b771fbfbcb3b9495374e4216f6dc0eb99e5
Alexander <aertius@gmail.com>
2018-09-29 20:59:08 +0600

    add solution of exercise 1.8

---
+++ exercises/1.8.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require rackunit)
+
+(define (cbrt-iter guess prev-guess x)
+  (if (good-enough? guess prev-guess)
+      guess
+      (cbrt-iter (improve guess x) guess
+                 x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square  guess)) (* 2  guess)) 3))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess prev-guess)
+  (< (abs (/ (- guess prev-guess) guess)) 0.00001))
+
+(define (square x) (* x x))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 0 x))
+
+(cbrt 8)
